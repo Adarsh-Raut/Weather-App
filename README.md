fdd7bb8cc15676f8af88d2e4d525232094b4d59b
Adarsh Raut <adarshraut0987@gmail.com>
2023-05-01 16:24:06 +0530

    Added failure.ejs and async await to api call

---
+++ README.md
@@ -0,0 +1,24 @@
+
+# Weather App using Node and Express
+
+This weather app provides you with temperature and weather details about the city you entered in the search bar.
+It gets the weather data from -->
+
+https://openweathermap.org/api
+
+and displays it on the page. I have used "ejs" for rendering data dynamically.
+
+## Steps to follow
+First to install all dependancies run this command -->
+    
+    npm install
+After finishing installing dependancies, Go to app.js and on line 13 where it says YOUR_API_KEY_HERE enter your own api key in single qoutes.
+
+After doing this you can run command -->
+
+    node app.js
+to run the server.
+
+You can check website at 
+
+localhost:3000
--- app.js
+++ app.js
@@ -1,43 +1,59 @@
-const express = require("express");
-const https = require("https");
-const bodyParser = require("body-parser");
-const { query } = require("express");
+const express = require('express');
+const https = require('https');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 
 const app = express();
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-const apiKey = "YOUR API KEY HERE";
-const unit = "metric";
+const apiKey = 'YOUR_API_KEY_HERE';
+const unit = 'metric';
 
-app.get("/", (req, res) => {
-  const sendData = { location: "Location", temp: "Temp", disc: "Description" };
-  res.render("index", { sendData: sendData });
+app.get('/', (req, res) => {
+  const sendData = { location: 'Location', temp: 'Temp', disc: 'Description' };
+  res.render('index', { sendData: sendData });
 });
 
-app.post("/", (req, res) => {
+app.post('/', (req, res) => {
   const query = req.body.cityName;
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${unit}`;
-  https.get(url, (response) => {
-    response.on("data", (data) => {
-      const weatherData = JSON.parse(data);
-      temperature = weatherData.main.temp;
-      weatherDescription = weatherData.weather[0].description;
-      const icon = weatherData.weather[0].icon;
-      const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-      const sendData = {
-        location: query,
-        temp: temperature,
-        desc: weatherDescription,
-        imageURL: imageURL,
-      };
-      res.render("index", { sendData: sendData });
-    });
-  });
+
+  async function doWork() {
+    try {
+      const response = await fetch(url);
+      console.log('Response received');
+      const data = await response.json();
+      console.log(data);
+      if (data.cod > 400) {
+        // msg = data.message;
+        console.log(data.message);
+        message = data.message;
+        res.render('failure', { message: message });
+      } else {
+        // return data;
+        const weatherData = data;
+        temperature = weatherData.main.temp;
+        weatherDescription = weatherData.weather[0].description;
+        const icon = weatherData.weather[0].icon;
+        const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+        const sendData = {
+          location: query,
+          temp: temperature,
+          desc: weatherDescription,
+          imageURL: imageURL,
+        };
+        res.render('index', { sendData: sendData });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  doWork();
 });
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000.");
+  console.log('Server is running on port 3000.');
 });
